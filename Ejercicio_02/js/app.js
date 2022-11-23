1c1570b679e17e00f07ec6e1347bb002753ee0b3
Adrian Barrero <abarrero@tensolutions.es>
2022-11-23 14:00:22 +0100

    seccion2

---
+++ Ejercicio_02/js/app.js
@@ -0,0 +1,30 @@
+let = app = angular.module('universidadApp', [] );
+
+app.controller('profesorCtrl', function($scope) {
+
+    $scope.profesor = profesorData;
+    $scope.editando = {};
+
+
+    $scope.EditarProfesor = function() {
+        angular.copy( $scope.profesor, $scope.editando);
+    }
+
+    $scope.GuardarCambios = function() {
+        angular.copy($scope.editando, $scope.profesor);
+        $scope.editando = {};
+    }
+
+    $scope.CancelarCambios = function() {
+        $scope.editando = {};
+    }
+
+
+});
+
+let profesorData = {
+	nombre: "Juan Carlos Pineda",
+	bio: "Saludos estudiante, mi nombre es Juan Carlos, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.",
+	edad: 47,
+	foto: "img/juancarlos.jpg"
+}
